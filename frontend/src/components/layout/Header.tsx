349ad5637b70796b290287d91357df5f39733c0e
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-13 21:05:17 +0200

    add burgerOption to Header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import Button from "../ui/Button";
+import Image from "next/image";
+import { FC } from "react";
 
-const Header = () => {
+interface HeaderProps {
+  burgerOption?: 'menu' | 'header';
+}
+
+const Header: FC<HeaderProps> = ({ burgerOption = 'menu' }) => {
   return (
-    <header className="header">
+    <header className="header" id="header">
       <nav className="navigation">
         <p className="typography__paragraph typography__paragraph--logo">English.io</p>
         <ul className="navigation__list">
@@ -19,14 +25,25 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-      <ul className="navigation__login">
+      <div className="navigation__login">
         <Link href='/login'>
           <Button title={"Login"} />
         </Link>
         <Link href='/login'>
           <Button title={"Sign Up"} />
         </Link>
-      </ul>
+      </div>
+      <a
+        className="navigation__menu"
+        href={`#${burgerOption}`}
+      >
+        <Image
+          src={"icon-menu.svg"}
+          alt={"menu"}
+          width={35}
+          height={35}
+        />
+      </a>
     </header>
   )
 };
