8affc8aa734bf3c4b4fe17a09b82b44f2a53868b
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-13 19:40:10 +0200

    implement Header to layout tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from '@/components/ui/Header';  
+import Header from '@/components/layout/Header';  
 
 const inter = Inter({ subsets: ["latin"] });
 
