8b37b8a0dd024f75a12e4375fc6a0fd0cbe0b31d
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-13 21:05:01 +0200

    implement menu to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Hero from '@/components/layout/Hero';
+import Menu from '@/components/layout/Menu';
 import '@/styles/main.scss';
 
 export default function Home() {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <Menu />
+    </>
   );
 }
