1b95da330868d6c885b86bcecd8b62e722b90ea1
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-14 15:37:29 +0200

    add OurService to Home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from '@/components/layout/Hero';
 import Menu from '@/components/layout/Menu';
-import '@/styles/main.scss';
+import OurService from '@/components/layout/OurService';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Menu />
+      <OurService />
     </>
   );
 }
