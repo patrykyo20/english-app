064a6cd6c04a1276f6a1ad8475fe30053cfa5da6
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-14 15:37:11 +0200

    add main.scss and footer to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from '@/components/layout/Header';  
+import Footer from "@/components/layout/Footer";
+import '@/styles/main.scss';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
