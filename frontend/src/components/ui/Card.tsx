bc98e4827ee01032b941aadf2fc06a5c6bcf0338
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-14 15:39:14 +0200

    create Card component

---
+++ frontend/src/components/ui/Card.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+interface CardProps {
+  variant: 'quiz' | 'sentences' | 'grammar';
+  description: string;
+}
+
+const Card: FC<CardProps> = ({
+  variant,
+  description,
+}) => {
+  return (
+    <article
+      className={
+        `card card--${variant}
+      `}
+    >
+      <Image
+        src={`/${variant}.svg`}
+        alt={`${variant}`}
+        width={150}
+        height={150}
+        className="card__image"
+      />
+      <h3 className="typography__paragraph--button typography__title--card card__title">{variant}</h3>
+      <p className="typography__paragraph--card typography--center card__paragraph">
+        {description}
+      </p>
+      <Link href={`/${variant}`} className="typography__paragraph--button card__link">Learn More &#8594;</Link>
+    </article>
+  );
+};
+
+export default Card;
