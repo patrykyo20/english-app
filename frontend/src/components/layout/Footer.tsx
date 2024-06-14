bdc485791e5efdcbbb7aebe1bf8f87db9c9ad898
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-14 15:37:17 +0200

    create Footer component

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,15 @@
+import Navigation from "../ui/Navigation";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Navigation variant={"light"} />
+
+      <div>
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
