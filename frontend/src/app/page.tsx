45a5f11528707de2fd72ffa812baa515c5c346b1
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-15 20:52:15 +0200

    test user and token

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import Hero from '@/components/layout/Hero';
 import Menu from '@/components/layout/Menu';
 import OurService from '@/components/layout/OurService';
+import { useUserContext } from '@/context/userProvider';
 
 export default function Home() {
+  const { user, token } = useUserContext();
+
+  console.log(user);
+  console.log(token);
+
   return (
     <>
       <Hero />
