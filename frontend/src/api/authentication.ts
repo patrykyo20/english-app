6620587ada4142880cb162d0c8502352d2057b1d
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-18 13:24:00 +0200

    add api functionality to auth

--- frontend/src/api/authentication.ts
+++ frontend/src/api/authentication.ts
@@ -1,11 +1,5 @@
 import axios from 'axios';
-
-const config = {
-  headers: {
-    'Authorization': 'Bearer 3950a7f9fa2615a23ecf25387f31482554966248ecada56d301b72e8da058ae358a6b824b67f740d6b9c8d1b87b82197d71db3fcf0f8b7e0f32fa7ba230e49ef51a633bfdba5df353478b91d1799899737edc964c4fb11c9a1756b94036ad93c159a4ea6ce55f5733b6f6b8c05346a83c90b7ba619741fa96b5d336870b72fcc'
-  }
-}
-
+import { getUser } from './users';
 const URL = 'http://localhost:1337/api/';
 
 export const signIn = async (authData: { email: string; password: string; }) => {
@@ -15,12 +9,43 @@ export const signIn = async (authData: { email: string; password: string; }) => 
       password: authData.password,
     })
 
-    console.log(response)
+    const userToken = response.data.jwt;
 
-    return response.data;
+    const userId = response.data.user.id
+
+    const user = await getUser(userId);
+
+    return [user, userToken];
   } catch (error) {
     console.log(error);
     throw error; 
-  }
-}
+  };
+};
 
+export const signUp = async  (
+  authData:
+  {
+    username?: string,
+    email: string;
+    password: string;
+  }
+) => {
+  try {
+    const response = await axios.post(URL + 'auth/local/register', {
+      username: authData.username,
+      email: authData.email,
+      password: authData.password,
+    })
+
+    const userToken = response.data.jwt;
+
+    const userId = response.data.user.id
+
+    const user = await getUser(userId);
+
+    return [user, userToken];
+  } catch (error) {
+    console.log(error);
+    throw error; 
+  };
+};
