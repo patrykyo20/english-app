79b4c5a0bb988d49d2395acc504120fa96802063
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-18 13:26:45 +0200

    add user image to Header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,24 +1,48 @@
+'use client';
+
 import Link from "next/link";
 import Button from "../ui/Button";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Navigation from "../ui/Navigation";
+import { useUserContext } from "@/context/userProvider";
 
 interface HeaderProps {
   burgerOption?: 'menu' | 'header';
 }
 
 const Header: FC<HeaderProps> = ({ burgerOption = 'menu' }) => {
+  const { user } = useUserContext();
+  const [client, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
   return (
     <header className="header" id="header">
       <Navigation variant={"dark"} />
       <div className="navigation__login">
-        <Link href='/login'>
-          <Button title={"Login"} />
-        </Link>
-        <Link href='/login'>
-          <Button title={"Sign Up"} />
-        </Link>
+        {client && user ? (
+          <Link href={`/user-page/${user.data.id}`}>
+            <Image
+              src={user.data?.image.formats.medium.url || '/avatar.svg'}
+              alt="avatar"
+              height={50}
+              width={50}
+              className="user--avatar"
+            />
+          </Link>
+        ) : (
+          <>
+            <Link href='/sign-in'>
+              <Button title={"Login"} />
+            </Link>
+            <Link href='/sign-up'>
+              <Button title={"Sign Up"} />
+            </Link>
+          </>
+        )}
       </div>
       <a
         className="navigation__menu"
@@ -32,7 +56,7 @@ const Header: FC<HeaderProps> = ({ burgerOption = 'menu' }) => {
         />
       </a>
     </header>
-  )
+  );
 };
 
 export default Header;
