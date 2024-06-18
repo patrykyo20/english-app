b43cc107c07581dcec635f50293b5f4ae29bbd83
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-18 13:24:13 +0200

    add Loader

---
+++ frontend/src/components/ui/Loader.tsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
