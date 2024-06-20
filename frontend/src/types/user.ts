9c6dd733e6fd795df00894d057a50070fb2c9e93
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-20 09:57:43 +0200

    implement url to User interface

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,56 +1,50 @@
 interface User {
-  id: number,
-  username: string,
-  email: string,
-  provider: string,
-  confirmed: boolean,
-  blocked: boolean,
-  createdAt: string,
-  updatedAt: string,
-  description: string,
-  role: {
-    id: number,
-    name: string,
-    description: string,
-    type: string,
-    createdAt: string,
-    updatedAt: string
-  },
-  quizzes: [
-    {
-      id: number,
-      createdAt: string,
-      updatedAt: string,
-      publishedAt: string,
-      topic: string
-    }
-  ],
-  image: {
-    id: number,
-    name: string,
-    alternativeText: null,
-    caption: null,
-    width: number,
-    height: number,
-    hash: string;
+  id: number;
+  username: string;
+  email: string;
+  password?: string;
+  description?: string;
+  url?: string;
+  role?: {
+    id: number;
+    name: string;
+    description: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+  quizzes?: {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    topic: string;
+  }[];
+  image?: {
     formats: {
       small: {
-        url: string,
-        width: string,
-        height: string,
+        url: string;
+        width: string;
+        height: string;
       };
       medium: {
-        url: string,
-        width: string,
-        height: string,
+        url: string;
+        width: string;
+        height: string;
       };
       thumbnail: {
-        url: string,
-        width: string,
-        height: string,
+        url: string;
+        width: string;
+        height: string;
       };
     };
-
+    hash: string;
+    id: number;
+    name: string;
+    alternativeText: null;
+    caption: null;
+    width: number;
+    height: number;
   };
 };
 
