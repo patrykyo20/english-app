e46f74946d6e202054e15992295c7673d6fa8091
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-20 09:55:53 +0200

    remove Menu from page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
 import Hero from '@/components/layout/Hero';
-import Menu from '@/components/layout/Menu';
 import OurService from '@/components/layout/OurService';
-import { useUserContext } from '@/context/userProvider';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Menu />
       <OurService />
     </>
   );
