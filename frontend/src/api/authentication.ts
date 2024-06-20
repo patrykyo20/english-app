6982b4b71baf6d6cac5227811a72b3f8a1cb47e3
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-20 09:55:00 +0200

    add changePassword functionality

--- frontend/src/api/authentication.ts
+++ frontend/src/api/authentication.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { getUser } from './users';
-const URL = 'http://localhost:1337/api/';
+
+const URL = 'http://localhost:1337/api/auth/';
 
 export const signIn = async (authData: { email: string; password: string; }) => {
   try {
-    const response = await axios.post(URL + 'auth/local', {
+    const response = await axios.post(URL + 'local', {
       identifier: authData.email,
       password: authData.password,
     })
@@ -23,19 +24,19 @@ export const signIn = async (authData: { email: string; password: string; }) => 
 };
 
 export const signUp = async  (
-  authData:
-  {
+  authData: {
     username?: string,
     email: string;
     password: string;
   }
 ) => {
   try {
-    const response = await axios.post(URL + 'auth/local/register', {
+    const response = await axios.post(URL + 'local/register', {
       username: authData.username,
       email: authData.email,
       password: authData.password,
     })
+
 
     const userToken = response.data.jwt;
 
@@ -47,5 +48,28 @@ export const signUp = async  (
   } catch (error) {
     console.log(error);
     throw error; 
+  };
+};
+
+export const changePassword = async (
+  currentPassword: string,
+  newPassword: string,
+  token: string
+) => {
+  try {
+    const response = await axios.post(URL + 'local/password-change', {
+      currentPassword: currentPassword,
+      newPassword: newPassword,
+      confirmPassword: newPassword
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    });
+
+    return response.data;
+  } catch (error: any) {
+    console.error('Error changing password:', error.response ? error.response.data : error.message);
+    throw error;
   };
 };
