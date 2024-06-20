c3fe5283b3ad6ac8b9fc2a2537faa7a21c5b755b
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-20 09:57:25 +0200

    create debounce func

---
+++ frontend/src/utils/debounce.ts
@@ -0,0 +1,15 @@
+const debounce = (func: Function, delay: number) => {
+  let timeoutId: NodeJS.Timeout;
+
+  return (...args: any[]) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+export default debounce;
