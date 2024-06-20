fd46a37eadc45a313767e0a3d4d95a46cdd37fd2
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-20 10:00:46 +0200

    add type to Button component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,18 +1,26 @@
 import { FC } from "react";
 
 interface ButtonProps {
+  variant?: string;
+  type?: "button" | "reset" | "submit" | undefined
   title: string;
-  onClick?: () => void;
+  onClick?: (e: any) => void;
   className?: string;
 }
 
 const Button: FC<ButtonProps> = ({
+  variant = "default",
+  type = "button",
   title,
   onClick,
   className,
 }) => {
   return (
-    <button onClick={onClick} className={`button ${className}`}>
+    <button
+      onClick={onClick}
+      className={`button button--${variant} ${className}`}
+      type={type}
+    >
       {title}
     </button>
   );
