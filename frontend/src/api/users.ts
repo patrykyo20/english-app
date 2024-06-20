fa3614739271086ed25443de24a5e6fcb8b69afd
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-06-20 09:55:33 +0200

    create update user functionality

--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -1,3 +1,4 @@
+import User from "@/types/user";
 import axios from "axios";
 
 const URL = 'http://localhost:1337/api/';
@@ -16,4 +17,24 @@ export const getUser = async (id: number) => {
     console.log(error);
     throw error; 
   };
+};
+
+export const updateUser = async (id: number, userData: User) => {
+  console.log(userData)
+  try {
+    const response = await axios.put(URL + 'users/' + id, {
+      username: userData.username,
+      email: userData.email,
+      description: userData.description,
+      url: userData.url,
+    }, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error updating user:', error);
+    throw error;
+  }
 };
